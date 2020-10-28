907371559b9d71d04d03467b80ff14f8a174027e
LeticiaBerdet <leticiaberdet@gmail.com>
2020-10-28 16:13:06 -0300

    adicionando a rota mensagem

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática!")
 })
 
-app.get('/sobre', function(req,res){
-    res.send("Essa pagina esta sendo desenvolvida por Leticia")
-})
-
 
 app.listen(3000,function(){
     console.log("Conexão Inicialização na porta 3000")
