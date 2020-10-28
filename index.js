dacbeabd7880a7cd6ceb0f78c6309db8999594d9
LeticiaBerdet <leticiaberdet@gmail.com>
2020-10-28 15:42:02 -0300

    Adição da rota mensagem

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req,res){
     res.send("Olá Diego!")
 });
 
+app.get('/mensagem', function(req,res){
+    res.send("Essa mensagem é automática!")
+})
+
 app.listen(3000,function(){
     console.log("Conexão Inicialização na porta 3000")
 });
