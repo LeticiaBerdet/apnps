72de4fbcaa71dd16514b505d6cf7e388af75099b
LeticiaBerdet <leticiaberdet@gmail.com>
2020-11-04 17:01:54 -0300

    usando ejs nas paginas

--- index.js
+++ index.js
@@ -14,9 +14,19 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/', function(req,res){
-    res.send("Olá Diego!")
-});
+    res.render('index.ejs' , {}) 
+})
+
+app.get('/usuarios', function(req,res){
+    res.render('usuarios.ejs' , {usuarios:[
+        {nome:'Leticia',email:'leticiaberdet@gmail.com'},
+        {nome:'Maria', email:'maria@gmail.com'},
+        {nome:'Tereza', email:'tereza@gmail.com'},
+        {nome:'Ana', email:'ana@gmail.com'}
+    ]}) 
+})
+
 
 app.listen(3000,function(){
     console.log("Conexão Inicialização na porta 3000")
-});
+})
